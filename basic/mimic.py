c0037414407084321d46015a2c2aa16e897fdb26
Deepak-Deepu <deepakcsgec@gmail.com>
2016-04-23 20:09:36 +0530

    mimic completed

--- basic/mimic.py
+++ basic/mimic.py
@@ -47,25 +47,37 @@ import sys
 
 def mimic_dict(filename):
   """Returns mimic dict mapping each word to list of words which follow it."""
-  # +++your code here+++
-  return
-
+  f=open(filename).read()	
+  dict2={}
+  list5=f.split()
+  x=""	
+  for word in list5:
+    if not x in dict2:
+      dict2[x]=[word]
+    else:
+      dict2[x].append(word)
+    
+    x=word
+  
+  return dict2 			
 
 def print_mimic(mimic_dict, word):
   """Given mimic dict and start word, prints 200 random words."""
-  # +++your code here+++
-  return
-
-
+  for i in range(200):
+    print word,
+    y=mimic_dict.get(word)
+    if not y:
+      y=mimic_dict['']	
+    word=random.choice(y)			
 # Provided main(), calls mimic_dict() and mimic()
 def main():
-  if len(sys.argv) != 2:
-    print 'usage: ./mimic.py file-to-read'
-    sys.exit(1)
+	if len(sys.argv) != 2:
+		print 'usage: ./mimic.py file-to-read'
+		sys.exit(1)
 
-  dict = mimic_dict(sys.argv[1])
-  print_mimic(dict, '')
-
+	dict = mimic_dict(sys.argv[1])
+	print_mimic(dict, '')
+#	print dict
 
 if __name__ == '__main__':
-  main()
+	main()
